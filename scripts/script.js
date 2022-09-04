38a6c277a2aa8e4c3a59a10f8e0a0aa94484d117
Timothy Horth <tjhorth@gmail.com>
2022-09-03 20:53:28 -0400

    Initial commit for files

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+google.charts.load("current", {
+  packages: ["geochart"],
+  mapsApiKey: "AIzaSyA8lHLmVMXQVVYiRuK6B8IVSzoHnJL-jg4",
+});
+google.charts.setOnLoadCallback(() => {
+  fetch("/JSON/data.JSON")
+    .then((res) => {
+      return res.json();
+    })
+    .then((output) => {
+      const data = JSON.parse(output);
+      let arr = [["Country", "Total Capacity [MW]", "Number of Farms"]];
+      Object.keys(data.country).forEach((key) => {
+        arr.push([key, data.capacity_mw[key], data.number_of_farms[key]]);
+      });
+      return arr;
+    })
+    .then((arr) => {
+      drawRegionsMap(arr);
+    })
+    .catch(() => {
+      console.log("did not work");
+    });
+
+  function drawRegionsMap(arr) {
+    console.log(arr);
+    var data = google.visualization.arrayToDataTable(arr);
+
+    var options = {
+      colorAxis: { colors: ["#F2FBEB", "#8cc962"] },
+      backgroundColor: "#FFFFFF",
+      datalessRegionColor: "#FFFFFF",
+    };
+
+    var chart = new google.visualization.GeoChart(
+      document.getElementById("regions_div")
+    );
+
+    chart.draw(data, options);
+  }
+});
